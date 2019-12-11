eb6f658b0589d6d3b8b287fa1cc1995d2d185bb2
Ahmad Madani <ahmadmadani@Ahmads-MacBook-Pro.local>
2019-12-11 10:34:48 +0100

    second commit

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ app.get("/*", (req, res) => {
   res.sendFile("index.html", { root: __dirname + "/client/build/" });
 });
 
-const { PORT } = process.env;
+const PORT = process.env.PORT || 5050;
 app.listen(PORT, () => {
   console.log(`on server ${PORT}`);
 });
